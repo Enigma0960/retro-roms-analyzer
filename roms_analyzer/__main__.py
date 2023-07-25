faab7f198696ea852c5ab6ca57a7e0775ed7a0ff
Supryadkin Igor <igorsupr@gmail.com>
2023-07-25 23:24:25 +0700

    find all files

--- roms_analyzer/__main__.py
+++ roms_analyzer/__main__.py
@@ -1,8 +1,11 @@
 import os
+import attr
 import glob
 import logging
 import pprint
 import argparse
+
+from typing import Union, Optional
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -16,15 +19,35 @@ HEADER_ExHiROM_ADDR = 0x40FFC0
 FILE_HEADER_SIZE = 512
 
 
+@attr.s()
+class SnesHeader:
+    cartridge_title = attr.ib(type=bytes, default=b'')
+    rom_speed = attr.ib(type=bytes, default=b'')
+    chipset = attr.ib(type=bytes, default=b'')
+    rom_size = attr.ib(type=bytes, default=b'')
+    ram_size = attr.ib(type=bytes, default=b'')
+    country = attr.ib(type=bytes, default=b'')
+    developer_id = attr.ib(type=bytes, default=b'')
+    rom_version = attr.ib(type=bytes, default=b'')
+    checksum = attr.ib(type=bytes, default=b'')
+    checksum_compliment = attr.ib(type=bytes, default=b'')
+    interrupt_vectors = attr.ib(type=bytes, default=b'')
+
+
 def get_app_param() -> argparse.Namespace:
     parser = argparse.ArgumentParser(prog='retro roms analyzer')
     parser.add_argument('input')
     return parser.parse_args()
 
 
-def get_files(path: str) -> list[str]:
+def get_files(path: str, ext: Union[str, list[str]]) -> list[str]:
+    res: list[str] = []
     path = os.path.abspath(path)
-    res = glob.glob(path)
+    if type(ext) is str:
+        ext = [ext]
+    for file_type in ext:
+        file_path = os.path.join(path, f"*.{file_type}")
+        res += glob.glob(file_path, recursive=True)
     return res
 
 
@@ -44,42 +67,29 @@ def extract_bytes(data: bytes, size) -> tuple[bytes, bytes]:
     return data, res
 
 
-def get_header(data: bytes, start: int) -> dict:
-    if len(data) < 1024512:
-        return {}
+def get_header(data: bytes, start: int) -> Optional[SnesHeader]:
+    header: Optional[SnesHeader] = SnesHeader()
 
     hdata = get_bytes(data=data, offset=start, size=53)
 
-    hdata, cartridge_title = extract_bytes(hdata, 21)
-    hdata, rom_speed = extract_bytes(hdata, 1)
-    hdata, chipset = extract_bytes(hdata, 1)
-    hdata, rom_size = extract_bytes(hdata, 1)
-    hdata, ram_size = extract_bytes(hdata, 1)
-    hdata, country = extract_bytes(hdata, 1)
-    hdata, developer_id = extract_bytes(hdata, 1)
-    hdata, rom_version = extract_bytes(hdata, 1)
-    hdata, checksum = extract_bytes(hdata, 2)
-    hdata, checksum_compliment = extract_bytes(hdata, 2)
-    _, interrupt_vectors = extract_bytes(hdata, 32)
+    hdata, header.cartridge_title = extract_bytes(hdata, 21)
+    hdata, header.rom_speed = extract_bytes(hdata, 1)
+    hdata, header.chipset = extract_bytes(hdata, 1)
+    hdata, header.rom_size = extract_bytes(hdata, 1)
+    hdata, header.ram_size = extract_bytes(hdata, 1)
+    hdata, header.country = extract_bytes(hdata, 1)
+    hdata, header.developer_id = extract_bytes(hdata, 1)
+    hdata, header.rom_version = extract_bytes(hdata, 1)
+    hdata, header.checksum = extract_bytes(hdata, 2)
+    hdata, header.checksum_compliment = extract_bytes(hdata, 2)
+    _, header.interrupt_vectors = extract_bytes(hdata, 32)
 
-    return {
-        'cartridge_title': cartridge_title,
-        'rom_speed': rom_speed,
-        'chipset': chipset,
-        'rom_size': rom_size,
-        'ram_size': ram_size,
-        'country': country,
-        'developer_id': developer_id,
-        'rom_version': rom_version,
-        'checksum': checksum,
-        'checksum_compliment': checksum_compliment,
-        'interrupt_vectors': interrupt_vectors,
-    }
+    return header
 
 
 if __name__ == '__main__':
     args = get_app_param()
-    file_list = get_files(args.input)
+    file_list = get_files(args.input, "smc")
 
     for file_name in file_list:
         _LOGGER.info(f'File:{file_name}')
@@ -100,12 +110,12 @@ if __name__ == '__main__':
         header_ExLoROM = get_header(data, start + HEADER_ExLoROM_ADDR)
         header_ExHiROM = get_header(data, start + HEADER_ExHiROM_ADDR)
 
-        _LOGGER.info('LoROM: ' + pprint.pformat(header_LoROM['cartridge_title']))
-        _LOGGER.info('HiROM: ' + pprint.pformat(header_HiROM['cartridge_title']))
-        _LOGGER.info('ExHiROM: ' + pprint.pformat(header_ExHiROM['cartridge_title']))
+        _LOGGER.info('LoROM: ' + pprint.pformat(header_LoROM.cartridge_title))
+        _LOGGER.info('HiROM: ' + pprint.pformat(header_HiROM.cartridge_title))
+        _LOGGER.info('ExHiROM: ' + pprint.pformat(header_ExHiROM.cartridge_title))
 
-        _LOGGER.info('LoROM checksum: ' + pprint.pformat(header_LoROM['checksum']))
-        _LOGGER.info('HiROM checksum: ' + pprint.pformat(header_HiROM['checksum']))
-        _LOGGER.info('ExHiROM checksum: ' + pprint.pformat(header_ExHiROM['checksum']))
+        _LOGGER.info('LoROM checksum: ' + pprint.pformat(header_LoROM.checksum))
+        _LOGGER.info('HiROM checksum: ' + pprint.pformat(header_HiROM.checksum))
+        _LOGGER.info('ExHiROM checksum: ' + pprint.pformat(header_ExHiROM.checksum))
 
         _LOGGER.info('\n\n')
